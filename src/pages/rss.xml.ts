feb51f6b2948fdc4a40690ec1edf883dff4ab2b5
Rodney Lab <codesign@rodneylab.com>
2023-04-21 18:21:00 +0100

    style: 🛁 linting

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,21 +1,23 @@
 import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 
-export async function get({ site }) {
+export const get: APIRoute = async function get({ site }) {
   const posts = await getCollection("blog");
+  const items = posts
+    .sort((a, b) => Date.parse(b.data.date) - Date.parse(a.data.date))
+    .map(({ data: { date, title, description }, slug }) => ({
+      title: title,
+      description,
+      link: `${site}${slug}`,
+      pubDate: new Date(date),
+    }));
 
   return rss({
     title: "Novelty Cycles",
     description: "Novelty Cycles Blog Posts",
-    site,
-    customData: `<language>en</language>`,
-    items: posts
-      .sort((a, b) => Date.parse(b.data.date!) - Date.parse(a.data.date!))
-      .map(({ data: { date, title, description }, slug }) => ({
-        title: title,
-        description,
-        link: `${site}${slug}/`,
-        pubDate: new Date(date),
-      })),
+    site: site.toString(),
+    customData: "<language>en</language>",
+    items,
   });
-}
+};
